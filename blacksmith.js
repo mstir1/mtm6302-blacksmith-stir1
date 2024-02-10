0d04355a70bbed474de19fa00c34ed400f5dcfbc
Matthew Stirling <stir0025@algonquinlive.com>
2024-02-10 16:28:36 -0500

    Finished Code

--- blacksmith.js
+++ blacksmith.js
@@ -1,97 +1,132 @@
 //variables
-let wood = 2
-let fireActive = false
+const ore = "ore";
+const wood = "wood";
+const supply_types = [ore, wood];
+const supply_costs = {
+ore: 3,
+wood: 1
+}
 
-let money = 0
-let ore = 0
+const sword = "sword";
+const axe = "axe";
+const weapon_types = [sword, axe];
+const weapon_price = { // Amount each weapon sells for
+sword: 5,
+axe: 4
+}
+const weapon_make_requirements = {
+sword: {ore: 2, wood: 1},
+axe: {ore: 1, wood: 2}
+}
+// Blacksmith's state
+let blacksmith = {
+  ore: 5,
+  wood: 10,
+  gold: 10,
+  fireActive: 0,
+  weapons: {
+    sword: 0,
+    axe: 0
+  }
+};
 
-let sword = 0
-let axe = 0
-
-
-// Functions
-
-function fire(){
-    if(fire === false){
-        fireActive = false
-        console.log("You have put out the fire!")
-    } else if (wood > 0){
-        fireActive = true
-        wood = wood - 1 
-        console.log("You have made fire!")
+function fire() {
+  if (blacksmith.fireActive === 0) {
+    blacksmith.fireActive = 1;
+    if (blacksmith.wood > 0) {
+      blacksmith.wood -= 1;
+      console.log("You have made fire!");
     } else {
-        console.log("You have no wood!")
+      console.log("You have no wood!");
     }
-}
-
-function buy(wood){
-  if (money = 1) {
-    wood = 1
-    console.log("Shop List: Wood = $1 Ore = $3")
-  } else if (money > 1) {
-    console.log("You don't have enough money")
+  } else {
+    blacksmith.fireActive = 0;
+    console.log("You have put out the fire!");
   }
 }
 
-function buy(ore){
-  if (money = 3) {
-  ore = 1 
-  console.log("Shop List: Wood = $1 Ore = $3")
-  } else if (money < 3){
-    console.log("You don't have enough money")
+function buy(item) {
+  if (blacksmith.fireActive === 1) {
+    console.log("You cannot buy when fire is active");
+  } else {
+    if (item === "wood") {
+      if (blacksmith.gold >= 1) {
+        blacksmith.wood += 1;
+        blacksmith.gold -= 1;
+        console.log("Bought wood for 1 gold.");
+      } else {
+        console.log("You don't have enough money to buy wood");
+      }
+    } else if (item === "ore") {
+      if (blacksmith.gold >= 3) {
+        blacksmith.ore += 1;
+        blacksmith.gold -= 3;
+        console.log("Bought ore for 3 gold.");
+      } else {
+        console.log("You don't have enough money to buy ore");
+      }
+    } else {
+      console.log("Cannot buy unknown item type: " + item);
+    }
   }
 }
-
-/**
- * make
- */
-function make(sword){
-  if (ore = 2, wood = 1){
-    console.log("You have made a sword")
-  } else if (ore < 2, wood < 1){
-    console.log("You need more materials to make a sword")
-  }
-}
-
-function make(axe){
-  if (ore = 1, wood = 2){
-    console.log("You have made an axe")
-  } else if (ore < 1, wood < 2){
-    console.log("You need more materials to make an axe")
-  }
-}
-
-/**
- * sell
- */
-function sell(sword){
-  if (sword = 1){
-    money = money + 5
-    console.log("You have sold a sword")
-  } else if (sword < 1){
-    console.log("You don't have a sword to sell")
-  }
-}
-
-function sell(axe){
-  if (axe = 1){
-    money = money + 4
-    console.log("You have sold a axe")
-  } else if (axe < 1){
-    console.log("You don't have an axe to sell")
+function make(weapon) {
+  if (weapon === "sword") {
+    if (blacksmith.ore >= weapon_make_requirements.sword.ore && blacksmith.wood >= weapon_make_requirements.sword.wood) {
+      blacksmith.ore -= weapon_make_requirements.sword.ore;
+      blacksmith.wood -= weapon_make_requirements.sword.wood;
+      blacksmith.weapons.sword++;
+      console.log("You have made a sword");
+    } else {
+      console.log("You need more materials to make a sword");
+    }
+  } else if (weapon === "axe") {
+    if (blacksmith.ore >= weapon_make_requirements.axe.ore && blacksmith.wood >= weapon_make_requirements.axe.wood) {
+      blacksmith.ore -= weapon_make_requirements.axe.ore;
+      blacksmith.wood -= weapon_make_requirements.axe.wood;
+      blacksmith.weapons.axe++;
+      console.log("You have made an axe");
+    } else {
+      console.log("You need more materials to make an axe");
+    }
+  } else {
+    console.log("Invalid weapon type");
   }
 }
 
 
-
-/**
- * inventory
- * Shows the players current inventory
- */
-function inventory(){
-
-
+function sell(weapon) {
+  if (weapon === "sword") {
+    if (sword > 0) {
+      sword--;
+      money = money + 5
+      console.log("You have sold a sword");
+    } else {
+      console.log("You don't have any swords to sell");
+    }
+  } else if (weapon === "axe") {
+    if (axe > 0) {
+      axe--;
+      money = money + 4
+      console.log("You have sold an axe");
+    } else {
+      console.log("You don't have any axes to sell");
+    }
+  } else {
+    console.log("Invalid weapon type");
+  }
 }
+
+function inventory() {
+  console.log("Inventory:");
+  console.log(" Ore: "+ blacksmith.ore);
+  console.log(" Wood: "+ blacksmith.wood);
+  console.log(" Gold: "+ blacksmith.gold);
+  console.log(" Weapons:");
+  for (const [weapon, quantity] of Object.entries(blacksmith.weapons)) {
+  console.log(" " + weapon + ": " + quantity);
+  }
+  }
 
 
 /**
